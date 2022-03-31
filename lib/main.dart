cd13c6d8c8fab40169b085ab28545f4e224cb567
Abdorre <Abderrahmanerehioui@gmail.com>
2022-03-31 21:41:12 +0100

    My commit

---
+++ lib/main.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp/dendronapp/generatedswipeleftwidget/GeneratedSwipeleftWidget.dart';
+import 'package:flutterapp/dendronapp/generatedhomescreenwidget/GeneratedHomescreenWidget.dart';
+import 'package:flutterapp/dendronapp/generatedggscrollvwidget/GeneratedGgscrollvWidget.dart';
+import 'package:flutterapp/dendronapp/generatedareasectionwidget/GeneratedAreaSectionWidget.dart';
+import 'package:flutterapp/dendronapp/generatedareasectionwidget1/GeneratedAreaSectionWidget1.dart';
+import 'package:flutterapp/dendronapp/generatedareasectionwidget2/GeneratedAreaSectionWidget2.dart';
+import 'package:flutterapp/dendronapp/generatedaccountwidget/GeneratedAccountWidget.dart';
+import 'package:flutterapp/dendronapp/generatedfiltrewidget1/GeneratedFiltreWidget1.dart';
+import 'package:flutterapp/dendronapp/generatedfiltrewidget3/GeneratedFiltreWidget3.dart';
+import 'package:flutterapp/dendronapp/generatedfiltrewidget5/GeneratedFiltreWidget5.dart';
+import 'package:flutterapp/dendronapp/generatedfiltrewidget7/GeneratedFiltreWidget7.dart';
+import 'package:flutterapp/dendronapp/generatedfiltrewidget9/GeneratedFiltreWidget9.dart';
+import 'package:flutterapp/dendronapp/generatedswiperightwidget/GeneratedSwiperightWidget.dart';
+import 'package:flutterapp/dendronapp/generatedsettingswidget/GeneratedSettingsWidget.dart';
+import 'package:flutterapp/dendronapp/generateddecicionsettingswidget/GeneratedDecicionsettingsWidget.dart';
+import 'package:flutterapp/dendronapp/generatedappdetailswidget/GeneratedAppdetailsWidget.dart';
+import 'package:flutterapp/dendronapp/generated3rdpagewidget/Generated3rdpageWidget.dart';
+import 'package:flutterapp/dendronapp/generated2ndpagewidget/Generated2ndpageWidget.dart';
+import 'package:flutterapp/dendronapp/generated2ndpagewidget1/Generated2ndpageWidget1.dart';
+import 'package:flutterapp/dendronapp/generated2ndpagewidget2/Generated2ndpageWidget2.dart';
+import 'package:flutterapp/dendronapp/generated1stpagewidget/Generated1stpageWidget.dart';
+import 'package:flutterapp/dendronapp/generated1stpagewidget1/Generated1stpageWidget1.dart';
+import 'package:flutterapp/dendronapp/generated1stpagewidget2/Generated1stpageWidget2.dart';
+import 'package:flutterapp/dendronapp/generated1stpagewidget3/Generated1stpageWidget3.dart';
+import 'package:flutterapp/dendronapp/generated1stpagewidget4/Generated1stpageWidget4.dart';
+import 'package:flutterapp/dendronapp/generated1stpagewidget5/Generated1stpageWidget5.dart';
+import 'package:flutterapp/dendronapp/generated1stpagewidget6/Generated1stpageWidget6.dart';
+import 'package:flutterapp/dendronapp/generatedprogressiondetailswidget/GeneratedProgressiondetailsWidget.dart';
+import 'package:flutterapp/dendronapp/generatedarchivelistwidget/GeneratedArchivelistWidget.dart';
+import 'package:flutterapp/dendronapp/generatedcarbonviewwidget1/GeneratedCarbonviewWidget1.dart';
+import 'package:flutterapp/dendronapp/generatedaddanim1widget/GeneratedAddanim1Widget.dart';
+import 'package:flutterapp/dendronapp/generatedaddanim2widget/GeneratedAddanim2Widget.dart';
+import 'package:flutterapp/dendronapp/generatedaddanim3widget/GeneratedAddanim3Widget.dart';
+import 'package:flutterapp/dendronapp/generatedaddaimation1widget/GeneratedAddaimation1Widget.dart';
+import 'package:flutterapp/dendronapp/generatedaddanimation2widget/GeneratedAddanimation2Widget.dart';
+import 'package:flutterapp/dendronapp/generatedaddanimation3widget/GeneratedAddanimation3Widget.dart';
+import 'package:flutterapp/dendronapp/generateddecisionlog1widget/GeneratedDecisionlog1Widget.dart';
+import 'package:flutterapp/dendronapp/generateddecisionlog2widget/GeneratedDecisionlog2Widget.dart';
+import 'package:flutterapp/dendronapp/generateddecisionlog3widget/GeneratedDecisionlog3Widget.dart';
+
+void main() {
+  runApp(DendronApp());
+}
+
+class DendronApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/GeneratedHomescreenWidget',
+      routes: {
+        '/GeneratedSwipeleftWidget': (context) => GeneratedSwipeleftWidget(),
+        '/GeneratedHomescreenWidget': (context) => GeneratedHomescreenWidget(),
+        '/GeneratedGgscrollvWidget': (context) => GeneratedGgscrollvWidget(),
+        '/GeneratedAreaSectionWidget': (context) =>
+            GeneratedAreaSectionWidget(),
+        '/GeneratedAreaSectionWidget1': (context) =>
+            GeneratedAreaSectionWidget1(),
+        '/GeneratedAreaSectionWidget2': (context) =>
+            GeneratedAreaSectionWidget2(),
+        '/GeneratedAccountWidget': (context) => GeneratedAccountWidget(),
+        '/GeneratedFiltreWidget1': (context) => GeneratedFiltreWidget1(),
+        '/GeneratedFiltreWidget3': (context) => GeneratedFiltreWidget3(),
+        '/GeneratedFiltreWidget5': (context) => GeneratedFiltreWidget5(),
+        '/GeneratedFiltreWidget7': (context) => GeneratedFiltreWidget7(),
+        '/GeneratedFiltreWidget9': (context) => GeneratedFiltreWidget9(),
+        '/GeneratedSwiperightWidget': (context) => GeneratedSwiperightWidget(),
+        '/GeneratedSettingsWidget': (context) => GeneratedSettingsWidget(),
+        '/GeneratedDecicionsettingsWidget': (context) =>
+            GeneratedDecicionsettingsWidget(),
+        '/GeneratedAppdetailsWidget': (context) => GeneratedAppdetailsWidget(),
+        '/Generated3rdpageWidget': (context) => Generated3rdpageWidget(),
+        '/Generated2ndpageWidget': (context) => Generated2ndpageWidget(),
+        '/Generated2ndpageWidget1': (context) => Generated2ndpageWidget1(),
+        '/Generated2ndpageWidget2': (context) => Generated2ndpageWidget2(),
+        '/Generated1stpageWidget': (context) => Generated1stpageWidget(),
+        '/Generated1stpageWidget1': (context) => Generated1stpageWidget1(),
+        '/Generated1stpageWidget2': (context) => Generated1stpageWidget2(),
+        '/Generated1stpageWidget3': (context) => Generated1stpageWidget3(),
+        '/Generated1stpageWidget4': (context) => Generated1stpageWidget4(),
+        '/Generated1stpageWidget5': (context) => Generated1stpageWidget5(),
+        '/Generated1stpageWidget6': (context) => Generated1stpageWidget6(),
+        '/GeneratedProgressiondetailsWidget': (context) =>
+            GeneratedProgressiondetailsWidget(),
+        '/GeneratedArchivelistWidget': (context) =>
+            GeneratedArchivelistWidget(),
+        '/GeneratedCarbonviewWidget1': (context) =>
+            GeneratedCarbonviewWidget1(),
+        '/GeneratedAddanim1Widget': (context) => GeneratedAddanim1Widget(),
+        '/GeneratedAddanim2Widget': (context) => GeneratedAddanim2Widget(),
+        '/GeneratedAddanim3Widget': (context) => GeneratedAddanim3Widget(),
+        '/GeneratedAddaimation1Widget': (context) =>
+            GeneratedAddaimation1Widget(),
+        '/GeneratedAddanimation2Widget': (context) =>
+            GeneratedAddanimation2Widget(),
+        '/GeneratedAddanimation3Widget': (context) =>
+            GeneratedAddanimation3Widget(),
+        '/GeneratedDecisionlog1Widget': (context) =>
+            GeneratedDecisionlog1Widget(),
+        '/GeneratedDecisionlog2Widget': (context) =>
+            GeneratedDecisionlog2Widget(),
+        '/GeneratedDecisionlog3Widget': (context) =>
+            GeneratedDecisionlog3Widget(),
+      },
+    );
+  }
+}
